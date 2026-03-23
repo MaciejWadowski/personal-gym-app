253d0fc9a1606fd8462b3f367512533ed621dc70
Maciej Wadowski <macwadowskii@gmail.com>
2026-03-23 17:58:55 +0100

    Initial Commit

---
+++ personal-gym-app/personal_gym_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  personal_gym_appApp.swift
+//  personal-gym-app
+//
+//  Created by Maciej Wadowski on 23/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct personal_gym_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
